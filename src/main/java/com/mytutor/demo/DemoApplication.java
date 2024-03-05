52657675518ef91744c172118cbd9fdfd38b32a6
sphiwe nyoni <67907125+snmyk@users.noreply.github.com>
2024-03-05 13:42:36 +0200

    main application file

---
+++ src/main/java/com/mytutor/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.mytutor.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+
+@SpringBootApplication
+@EnableWebSecurity
+public class DemoApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	
+
+}
